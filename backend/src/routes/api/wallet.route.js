e466634afe36c266ef51f3283cc0a5ced2bce483
dn7manz <153960298+dn7manz@users.noreply.github.com>
2023-12-17 16:47:58 +0100

    Update wallet.route.js
    refactoring for code readability

--- backend/src/routes/api/wallet.route.js
+++ backend/src/routes/api/wallet.route.js
@@ -18,20 +18,59 @@ const {
     getTransactionSchema
 } = require('../../middleware/validators/walletValidator.middleware');
 
-router.get('/gettoptokens', awaitHandlerFactory(WalletController.getTopTokens));
-router.post('/create', auth(), createWalletSchema, awaitHandlerFactory(WalletController.createWallet));
-router.post('/access/privatekey', auth(), accessWalletWithPrivateKeySchema, awaitHandlerFactory(WalletController.accessWalletByPrivateKey));
-router.post('/access/keyphrase', auth(), accessWalletWithKeyPhraseSchema, awaitHandlerFactory(WalletController.accessWalletWithKeyphrase));
-router.post('/addtoken', auth(), addTokenSchema, awaitHandlerFactory(WalletController.addToken));
-router.post('/getbalance', auth(), getBalanceSchema, awaitHandlerFactory(WalletController.getBalance));
-router.post('/getprice', auth(), getPriceSchema, awaitHandlerFactory(WalletController.getPrice));
-router.post('/getassets', auth(), getAssetsSchema, awaitHandlerFactory(WalletController.getAssets));
-router.post('/sendtoken', auth(), sendTokenSchema, awaitHandlerFactory(WalletController.sendToken));
-router.post('/receivetoken', auth(), awaitHandlerFactory(WalletController.receiveToken));
-router.post('/getsecret', auth(), geSecretSchema, awaitHandlerFactory(WalletController.getSecret));
-router.post('/gettransaction', auth(), getTransactionSchema, awaitHandlerFactory(WalletController.getTransaction));
-router.post('/getadmintransaction', auth(Role.Super), awaitHandlerFactory(WalletController.getAdminTransaction));
-router.post('/getcoinfortransaction', auth(), awaitHandlerFactory(WalletController.getCoinForTransaction));
-router.get('/getwalletbyemail/:email', auth(Role.Super), awaitHandlerFactory(WalletController.getWalletByEmail));
+router.get('/gettoptokens',
+           awaitHandlerFactory(WalletController.getTopTokens));
+router.post('/create',
+            auth(),
+            createWalletSchema,
+            awaitHandlerFactory(WalletController.createWallet));
+router.post('/access/privatekey',
+            auth(),
+            accessWalletWithPrivateKeySchema,
+            awaitHandlerFactory(WalletController.accessWalletByPrivateKey));
+router.post('/access/keyphrase',
+            auth(),
+            accessWalletWithKeyPhraseSchema,
+            awaitHandlerFactory(WalletController.accessWalletWithKeyphrase));
+router.post('/addtoken',
+            auth(),
+            addTokenSchema,
+            awaitHandlerFactory(WalletController.addToken));
+router.post('/getbalance',
+            auth(),
+            getBalanceSchema,
+            awaitHandlerFactory(WalletController.getBalance));
+router.post('/getprice',
+            auth(),
+            getPriceSchema,
+            awaitHandlerFactory(WalletController.getPrice));
+router.post('/getassets',
+            auth(),
+            getAssetsSchema,
+            awaitHandlerFactory(WalletController.getAssets));
+router.post('/sendtoken',
+            auth(),
+            sendTokenSchema,
+            awaitHandlerFactory(WalletController.sendToken));
+router.post('/receivetoken',
+            auth(),
+            awaitHandlerFactory(WalletController.receiveToken));
+router.post('/getsecret',
+            auth(),
+            geSecretSchema,
+            awaitHandlerFactory(WalletController.getSecret));
+router.post('/gettransaction',
+            auth(),
+            getTransactionSchema,
+            awaitHandlerFactory(WalletController.getTransaction));
+router.post('/getadmintransaction',
+            auth(Role.Super),
+            awaitHandlerFactory(WalletController.getAdminTransaction));
+router.post('/getcoinfortransaction',
+            auth(),
+            awaitHandlerFactory(WalletController.getCoinForTransaction));
+router.get('/getwalletbyemail/:email',
+           auth(Role.Super),
+           awaitHandlerFactory(WalletController.getWalletByEmail));
 
 module.exports = router;
