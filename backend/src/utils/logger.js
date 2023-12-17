deafde56bfcbd86c0551961bd7eb29e7356cc5e8
dn7manz <153960298+dn7manz@users.noreply.github.com>
2023-12-17 17:49:00 +0100

    Update logger.js

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -4,8 +4,9 @@ const winston = require('winston');
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
+
   ],
   format: winston.format.simple()
 });
