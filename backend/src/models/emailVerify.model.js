9ac54314cfa5704977918a774d3d0d4a2d95ba56
dn7manz <153960298+dn7manz@users.noreply.github.com>
2023-12-17 04:37:58 +0100

    Update emailVerify.model.js
    Handling of Empty Results: 
    DELETE Statement Correction
    Consistent Error Handling

--- backend/src/models/emailVerify.model.js
+++ backend/src/models/emailVerify.model.js
@@ -1,70 +1,78 @@
-/* learn more: https://github.com/testing-library/jest-dom // @testing-library/jest-dom library provides a set of custom jest matchers that you can use to extend jest. These will make your tests more declarative, clear to read and to maintain.*/
-
 const query = require('../db/db-connection');
 const { multipleColumnSet } = require('../utils/common.utils');
-const Role = require('../utils/userRoles.utils');
 const HttpException = require('../utils/HttpException.utils');
+
 class EmailVerifyModel {
     tableName = 'email_verify';
 
     findOne = async (params) => {
         try {
-            const { columnSet, values } = multipleColumnSet(params)
-            
+            const { columnSet, values } = multipleColumnSet(params);
+
             const sql = `SELECT * FROM ${this.tableName}
-            WHERE ${columnSet}`;
+                         WHERE ${columnSet}`;
 
             const result = await query(sql, [...values]);
-            // return back the first row (user)
+
+            if (result.length === 0) {
+                // Handle the case of an empty result if needed
+                console.log('No results found.');
+            }
+
             return result[0];
-        } catch(error) {
-            return {error:error.sqlMessage}
+        } catch (error) {
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Error fetching data from the database', error);
         }
     }
 
     create = async ({ email, verify_code }) => {
         try {
             const sql = `INSERT INTO ${this.tableName}
-            (email, verify_code) VALUES (?,?)`;
+                         (email, verify_code) VALUES (?,?)`;
 
             const result = await query(sql, [email, verify_code]);
             const affectedRows = result ? result.affectedRows : 0;
 
             return affectedRows;
         } catch (error) {
-            return {error:error.sqlMessage}
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Error creating a new record', error);
         }
     }
 
     update = async (params, id) => {
         try {
-            const { columnSet, values } = multipleColumnSet(params)
-            
+            const { columnSet, values } = multipleColumnSet(params);
+
             const sql = `UPDATE ${this.tableName} SET ${columnSet} WHERE id = ?`;
-            
+
             const result = await query(sql, [...values, id]);
 
             return result;
-        } catch(error) {
-            return {error:error.sqlMessage}
+        } catch (error) {
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Error updating the record', error);
         }
     }
 
     delete = async (params) => {
         try {
-            const { columnSet, values } = multipleColumnSet(params)
-            
-            const sql = `DELETE * FROM ${this.tableName}
-            WHERE ${columnSet}`;
+            const { columnSet, values } = multipleColumnSet(params);
+
+            // Fix: Corrected the DELETE statement
+            const sql = `DELETE FROM ${this.tableName}
+                         WHERE ${columnSet}`;
 
             const result = await query(sql, [...values]);
             const affectedRows = result ? result.affectedRows : 0;
 
             return affectedRows;
         } catch (error) {
-            return {error:error.sqlMessage}
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Error deleting the record', error);
         }
     }
 }
 
-module.exports = new EmailVerifyModel;
+module.exports = new EmailVerifyModel();
