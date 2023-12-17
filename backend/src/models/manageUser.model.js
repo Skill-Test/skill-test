96ef2e867bbcdb8fdde4d4d27066c2f8e7ba91fc
dn7manz <153960298+dn7manz@users.noreply.github.com>
2023-12-17 13:59:59 +0100

    Update manageUser.model.js
    Error Handling with throw: Error handling using throw has been added instead of returning error objects.
    
    Winston logging: Added error logging using Winston.

--- backend/src/models/manageUser.model.js
+++ backend/src/models/manageUser.model.js
@@ -2,8 +2,9 @@
 
 const query = require('../db/db-connection');
 const { multipleColumnSet } = require('../utils/common.utils');
-const Role = require('../utils/userRoles.utils');
+const winston = require('../utils/logger.js');
 const HttpException = require('../utils/HttpException.utils');
+
 class ManageUserModel {
     tableName = 'manage_user';
 
@@ -11,67 +12,76 @@ class ManageUserModel {
         try {
             let sql = `SELECT * FROM ${this.tableName} WHERE id!=1 ORDER BY ${sortBy} ${direction}`;
             return await query(sql);
-        } catch(error) {
-            return {error:error.sqlMessage};
+        } catch (error) {
+            // Handle error and register in Winston
+            winston.error(`[ManageUserModel] Find Error: ${error.message}`);
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Internal server error');
         }
     }
 
     findOne = async (params) => {
         try {
-            const { columnSet, values } = multipleColumnSet(params)
+            const { columnSet, values } = multipleColumnSet(params);
             
-            const sql = `SELECT * FROM ${this.tableName}
-            WHERE ${columnSet}`;
+            const sql = `SELECT * FROM ${this.tableName} WHERE ${columnSet}`;
             const result = await query(sql, [...values]);
 
             // return back the first row (user)
             return result[0];
-        } catch(error) {
-            return {error:error.sqlMessage};
+        } catch (error) {
+            // Handle error and register in Winston
+            winston.error(`[ManageUserModel] FindOne Error: ${error.message}`);
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Internal server error');
         }
     }
     
     create = async ({ email, have_wallet, wallet_address }) => {
         try {
-            const sql = `INSERT INTO ${this.tableName}
-            (email, have_wallet, wallet_address) VALUES (?,?,?)`;
-
+            const sql = `INSERT INTO ${this.tableName} (email, have_wallet, wallet_address) VALUES (?,?,?)`;
             const result = await query(sql, [email, have_wallet, wallet_address]);
             const affectedRows = result ? result.affectedRows : 0;
 
             return affectedRows;
         } catch (error) {
-            return {error:error.sqlMessage};
+            // Handle error and register in Winston
+            winston.error(`[ManageUserModel] Create Error: ${error.message}`);
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Internal server error');
         }
     }
 
     update = async (params, id) => {
         try {
-            const { columnSet, values } = multipleColumnSet(params)
+            const { columnSet, values } = multipleColumnSet(params);
 
             const sql = `UPDATE ${this.tableName} SET ${columnSet} WHERE id = ?`;
-
             const result = await query(sql, [...values, id]);
 
             return result;
-        } catch(error) {
-            return {error:error.sqlMessage};
+        } catch (error) {
+            // Handle error and register in Winston
+            winston.error(`[ManageUserModel] Update Error: ${error.message}`);
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Internal server error');
         }
     }
 
     delete = async (params) => {
         try {
-            const { columnSet, values } = multipleColumnSet(params)
-            
-            const sql = `DELETE FROM ${this.tableName}
-            WHERE ${columnSet}`;
+            const { columnSet, values } = multipleColumnSet(params);
+            const sql = `DELETE FROM ${this.tableName} WHERE ${columnSet}`;
 
             const result = await query(sql, [...values]);
             const affectedRows = result ? result.affectedRows : 0;
 
             return affectedRows;
         } catch (error) {
-            return {error:error.sqlMessage};
+            // Handle error and register in Winston
+            winston.error(`[ManageUserModel] Delete Error: ${error.message}`);
+            // Throw an exception with a descriptive message on error
+            throw new HttpException(500, 'Internal server error');
         }
     }
 }
